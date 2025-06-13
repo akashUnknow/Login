add71954471242fbb5387226024d32f16e724799
akashUnknow <akash.unknow@gmail.com>
2025-06-13 04:38:49 +0000

    init2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,25 @@
-
-import React from 'react'
-
-const App = () => {
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
+export default function App() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-gray-800">
-      hello
-      </div>
-    
-  )}
-export default App
+    <BrowserRouter>
+      {/* header */}
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
