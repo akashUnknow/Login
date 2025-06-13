8ef64ede2503726a837c9eba5895ccb8014d0563
akashUnknow <akash.unknow@gmail.com>
2025-06-13 04:23:55 +0000

    install

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-gray-800">
+      hello
+      </div>
+    
+  )}
+export default App
